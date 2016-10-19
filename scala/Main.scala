bf75269e47403cd67c7367df4d0a9fb42c03a208
vincent <vincent@gephen.co>
2016-10-19 12:32:11 +0800

    scala full rewrite

--- scala/Main.scala
+++ scala/Main.scala
@@ -1,172 +1,112 @@
+/**
+  * Created by vincentdupont on 18/10/16.
+  */
+
 import scala.io.Source
 import java.text.SimpleDateFormat
-import scala.math.{pow, sqrt, abs}
+import java.util.Date
+import scala.annotation.tailrec
+import scala.math.{pow, sqrt}
+import scala.math.Ordering.Implicits._
+
 
 object Main {
 
-  type User = String
+  type X = Double
+  type Y = Double
   type Floor = Int
-  type TimeStamp = Long
-  type Distance = Double
-  type Result = (Distance, Record, Record)
+  type Hours = Int
+  type Chunk = Int
+  type Key = (Floor, Hours, Chunk)
+  type Value = (Int, X, Y)
+  type UMap = Map[Key, Value]
 
-  class Record(val x: Double, val y: Double, val floor: Floor, val timeStamp: TimeStamp, val user: User) {
-    override def toString: String =
-      s"Record($x, $y, $floor, $timeStamp, $user)"
 
-    def distance(other: Record) : Distance = {
-      sqrt(pow(other.x - x, 2) + pow(other.y - y, 2))
+  def makeKey(d: Date, f: Floor): Key = {
+    val minutes = d.getMinutes
+    val hours = if (d.getDate == 19) 0 else 24
+    minutes match {
+      case _ if minutes < 10 => (f, d.getHours + hours, 0)
+      case _ if minutes < 20 => (f, d.getHours + hours, 1)
+      case _ if minutes < 30 => (f, d.getHours + hours, 2)
+      case _ if minutes < 40 => (f, d.getHours + hours, 3)
+      case _ if minutes < 50 => (f, d.getHours + hours, 4)
+      case _ => (f, d.getHours + hours, 5 )
     }
   }
 
+  def updateMap(m: UMap, key: Key, x: X, y: Y) : UMap = {
+    def calcMean(item : (Int, X, Y), x: X, y: Y) : Value = item match {
+      case (i, xx, yy) => (i+1, (xx*i+x) / (i+1), (yy*i + y) / (i+1))
+    }
+    if (m.contains(key))
+      m + (key -> calcMean(m(key), x, y))
+    else
+      m + (key -> (1, x, y))
+  }
 
-  def bruteForce(userA : List[Record], userB : List[Record], timeFrame: Long, withinDistance: Distance) : Result = {
-    var minDistance : Distance = Long.MaxValue
-    var distance : Distance = 0
-    var timeDelta : Long = 0
-    var result : Result = (minDistance, null, null)
-    for (r1 <- userA) {
-      for (r2 <- userB) {
-        timeDelta = abs(r1.timeStamp - r2.timeStamp)
-        distance = r1.distance(r2)
-        if (timeDelta < timeFrame && distance < minDistance) {
-          minDistance = distance
-          result = (minDistance, r1, r2)
-          if (result._1 < withinDistance) {
-            return result
-          }
-        }
+  def distance(a : Value, b : Value) : Double = a match {
+    case (_, x1, y1) => b match {
+      case (_, x2, y2) => sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))
+    }
+  }
+
+  @tailrec
+  def process(userAmap: UMap, userBmap: UMap, userAkeys : List[Key], userBkeys : List[Key]) : Key = userAkeys match {
+    case aKey :: xs =>
+      userBkeys match {
+        case bKey :: ys if aKey < bKey =>
+          process(userAmap, userBmap, xs, userBkeys)
+        case bKey :: ys if aKey > bKey =>
+          process(userAmap, userBmap, userAkeys, ys)
+        case bKey :: ys if aKey == bKey =>
+          if (distance(userAmap(aKey) , userBmap(bKey)) < 10) bKey else process(userAmap, userBmap, xs, ys)
+        case _ => (0, 0, 0)
       }
-    }
-    result
+    case _ => (0, 0, 0)
   }
 
-  def stripClosest(userA : List[Record], userB : List[Record], d : Result, timeFrame: Long, withinDistance: Distance) : Result = {
-    if (d._1 < withinDistance) return d
-    var minDistance : Distance = Long.MaxValue
-    var result : Result = (minDistance, null, null)
-    var distance : Distance = 0
-    var timeDelta : Long = 0
-    val userAYsorted = userA.sortBy(_.y)
-    val userBYsorted = userB.sortBy(_.y)
-    for (r1 <- userAYsorted) {
-      for (r2 <- userBYsorted) {
-        if (abs(r1.y - r2.y) < minDistance) return result
-        timeDelta = abs(r1.timeStamp - r2.timeStamp)
-        distance = r1.distance(r2)
-        if (timeDelta < timeFrame && distance < minDistance) {
-          minDistance = distance
-          result = (minDistance, r1, r2)
-          if (result._1 < withinDistance) {
-            return result
-          }
-        }
-      }
+  def output(userA : String, userB : String,  key: Key) : Unit = {
+    if (key._1 == 0) println(s"$userA and $userB didn't actually met")
+    else {
+      val day = if (key._2 > 24) "20" else "19"
+      val hour = if (key._2 > 24) key._2 - 24 else key._2
+      val chunk = key._3
+      println(s"$userA and $userB met at the ${key._1} floor on the ${day}th between $hour:${chunk*10} and $hour:${chunk*10+9}")
     }
-    result
   }
 
-  def findClosest(userA : List[Record], userB : List[Record], timeFrame: Long, withinDistance: Distance) : Result = {
-
-    val sizeA = userA.length
-    val sizeB = userB.length
-    if (sizeA <= 3 || sizeB <= 3) {
-      bruteForce(userA, userB, timeFrame, withinDistance)
-    } else {
-
-      // find middle records
-      val middleA : Int = sizeA / 2
-      val middleB : Int = sizeB / 2
-      val middleARecord : Record = userA(middleA)
-      val middleBRecord : Record = userB(middleB)
-      //    return (0, new Record(0, 0, 0, 0, ""), new Record(0, 0, 0, 0, ""))
-
-      val dl : Result = findClosest(userA.slice(0, middleA), userB.slice(0, middleB), timeFrame, withinDistance)
-      val dr : Result = findClosest(userA.slice(middleA, sizeA), userB.slice(middleB, sizeB), timeFrame, withinDistance)
-
-      val d : Result = if (dl._1 < dr._1) dl else dr
-
-      // build strip array
-      val stripA : List[Record] = userA.foldLeft(List[Record]()) ((acc, record) => {
-        if (abs(record.x - middleARecord.x) < d._1 ) record :: acc else acc
-      })
-
-      val stripB : List[Record] = userB.foldLeft(List[Record]()) ((acc, record) => {
-        if (abs(record.x - middleBRecord.x) < d._1 ) record :: acc else acc
-      })
-
-      val stripped : Result = stripClosest(stripA, stripB, d, timeFrame, withinDistance)
-
-      if (d._1 < stripped._1) d else stripped
-    }
-
-  }
 
   def main(args: Array[String]): Unit = {
-
-    // TODO arguments --user-a --user-b --file | all mandatory and user a != user b
-
-    if (args.length < 3) {
-      println("program should have three positional arguments:")
-      println("scala Main user_1 user_2 file.csv")
-      return
-    }
     val userA = args(0)
     val userB = args(1)
-
-    if (userA == userB) {
-      println("the two users should be different")
-      println("scala Main user_1 user_2 file.csv")
-    }
-
     val filename = args(2)
+
+    // create chunk of time, let's say 6 by hours by people and by floor
+
     val dateParse = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX")
 
-    try {
-
-      val init : Map[User, Map[Floor, List[Record]]] = Map(
-        userA -> Map(1 -> List[Record](), 2 -> List[Record](), 3 -> List[Record]()),
-        userB -> Map(1 -> List[Record](), 2 -> List[Record](), 3 -> List[Record]()))
-
-      // Reading the dataset and pre processing it
-      val data = Source.fromFile(filename).getLines().foldLeft (init)  {
-        (acc, line) => {
-          val splitted = line split ","
-          if (splitted(0) == "timestamp") acc
-          else {
-            val user = splitted(4) // user
-            if (user == userA || user == userB) {
-              val timeStamp = dateParse.parse(splitted(0)).getTime   // timestamp
-              val x = splitted(1).toDouble // x
-              val y = splitted(2).toDouble // y
-              val floor = splitted(3).toInt // floor
-              val record = new Record(x, y, floor, timeStamp, user)
-              acc.updated(user, acc(user) updated(floor, record :: acc(user)(floor)))
-            } else {
-              acc
-            }
+    def preProcess(acc: (UMap, UMap), line: String): (UMap, UMap) = {
+      val splitted = line split ","
+      if (splitted(0) == "timestamp") acc
+      else {
+        val user = splitted(4) // user
+        if (user == userA || user == userB) {
+          val timeStamp = dateParse.parse(splitted(0)) // timestamp
+          val x = splitted(1).toDouble // x
+          val y = splitted(2).toDouble // y
+          val floor = splitted(3).toInt // floor
+          val key = makeKey(timeStamp, floor)
+          if (user == userA) {
+            (updateMap(acc._1, key, x, y), acc._2)
+          } else {
+            (acc._1, updateMap(acc._2, key, x, y))
           }
-        }
+        } else acc
       }
-
-      val toSort : List[(User, Floor)]= for (u <- List(userA, userB); f <- 1 to 3) yield (u, f)
-
-      val sortedData = toSort.foldLeft(data) ((acc, value) => {
-        acc.updated(value._1, acc(value._1) updated(value._2, acc(value._1)(value._2).sortBy(_.x) ))
-      })
-
-//      val date = new java.util.Date()
-
-      val result = (1 to 3).foldLeft( (Double.MaxValue, new Record(0, 0, 0, 0, ""), new Record(0, 0, 0, 0, "")) ) ((acc, floor) => {
-        if (acc._1 < 10) acc else findClosest(sortedData(userA)(1), sortedData(userB)(1), 300*1000, 10)
-      })
-
-      println(result)
-      println(if (result._1 < 10) "These two people met" else "These two people didn't met")
-//      println("process duration :", new java.util.Date().getTime - date.getTime)
-
-    } catch {
-      case ex: Exception => println("An exception has occurred when opening the file.", ex)
     }
+
+    val (userAmap, userBmap) = Source.fromFile(filename).getLines().foldLeft((Map[Key, Value](), Map[Key, Value]()))(preProcess)
+    output(userA, userB, process(userAmap, userBmap, userAmap.keys.toList.sorted, userBmap.keys.toList.sorted))
   }
 }
